2583c1f5aa3d8d74c9ff4e33242121ddc75be921
mehedi <mehedi25122003@gmail.com>
2025-02-17 23:38:24 +0600

    card

--- script.js
+++ script.js
@@ -1,30 +1,47 @@
-const loadCategories = () =>{
-    fetch('https://openapi.programming-hero.com/api/phero-tube/categories')
-    .then(res => res.json())
-    .then(data => displayData(data.categories))
-}
+const loadCategories = () => {
+  fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
+    .then((res) => res.json())
+    .then((data) => displayData(data.categories));
+};
 
 const displayData = (category) => {
-    category.forEach(item => {
-        const buttonContainer = document.getElementById('btn-container');
-        const button = document.createElement('button');
-        button.className = 'px-3 py-1 text-textCl rounded-md font-thin bg-littleBg';
-        button.innerText = item.category;
-        buttonContainer.appendChild(button);
-    });
-}
+  category.forEach((item) => {
+    const buttonContainer = document.getElementById("btn-container");
+    const button = document.createElement("button");
+    button.className = "px-3 py-1 text-textCl rounded-md font-thin bg-littleBg";
+    button.innerText = item.category;
+    buttonContainer.appendChild(button);
+  });
+};
 loadCategories();
 
-
 const loadVideos = () => {
-    fetch('https://openapi.programming-hero.com/api/phero-tube/videos')
-    .then(res => res.json())
-    .then(data => loadvideoDisplay(data))
-}
+  fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
+    .then((res) => res.json())
+    .then((data1) => loadvideoDisplay(data1.videos));
+};
 
-const loadvideoDisplay = (video) =>{
-    video.forEach(item =>{
-        console.log(item)
-    })
-}
-loadVideos()
+const loadvideoDisplay = (video) => {
+  console.log(video);
+  video.forEach((item) => {
+    const container = document.getElementById("videos-container");
+    const divVideo = document.createElement("div");
+    divVideo.className = "card card-compact";
+    divVideo.innerHTML = `
+   <figure>
+     <img
+      src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+      alt="Shoes" />
+   </figure>
+  <div class="card-body">
+    <h2 class="card-title">Shoes!</h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <div class="card-actions justify-end">
+      <button class="btn btn-primary">Buy Now</button>
+    </div>
+  </div>`;
+  container.append(divVideo);
+
+  });
+};
+loadVideos();
