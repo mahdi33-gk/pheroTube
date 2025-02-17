99f32c235f638ebe787547bb92a317abe965bb85
mehedi <mehedi25122003@gmail.com>
2025-02-17 23:10:34 +0600

    not working 2nd function

--- script.js
+++ script.js
@@ -8,9 +8,23 @@ const displayData = (category) => {
     category.forEach(item => {
         const buttonContainer = document.getElementById('btn-container');
         const button = document.createElement('button');
-        button.className = 'px-3 py-1 text-textCl bg-primary text-white rounded-md  bg-littleBg';
+        button.className = 'px-3 py-1 text-textCl rounded-md font-thin bg-littleBg';
         button.innerText = item.category;
         buttonContainer.appendChild(button);
     });
 }
-loadCategories()
+loadCategories();
+
+
+const loadVideos = () => {
+    fetch('https://openapi.programming-hero.com/api/phero-tube/videos')
+    .then(res => res.json())
+    .then(data => loadvideoDisplay(data))
+}
+
+const loadvideoDisplay = (video) =>{
+    video.forEach(item =>{
+        console.log(item)
+    })
+}
+loadVideos()
